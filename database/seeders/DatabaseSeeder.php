b1eb4ca988b7821427ebb26502d31d5fae3adce2
mls42 <laspina92@gmail.com>
2024-12-22 15:20:07 +0100

    Optimize Recipes.vue and add Recipes to Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,37 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'id' => 1,
-            'name' => 'Michl',
-            'email' => 'm@mail.com',
-            'password'=> bcrypt('123123123')
+            'id'        => 1,
+            'name'      => 'Michl',
+            'email'     => 'm@mail.com',
+            'password'  => bcrypt('123123123')
+        ]);
+
+        Recipe::factory()->create([
+            'id'          => 1,
+            'title'       => 'Spaghetti Carbonara',
+            'ingredients' => ['Spaghetti', 'Eggs', 'Parmesan', 'Bacon', 'Black Pepper'],
+            'preparation' => 'Boil spaghetti. Cook bacon. Mix eggs with parmesan and combine with cooked spaghetti and bacon.',
+            'difficulty'  => 'medium',
+            'time'        => '30',
+        ]);
+
+        Recipe::factory()->create([
+            'id'          => 2,
+            'title'       => 'Pancakes',
+            'ingredients' => ['Milk', 'Flour', 'Eggs', 'Butter'],
+            'preparation' => 'Mix everything and then fry it in a pan.',
+            'difficulty'  => 'easy',
+            'time'        => '10',
+        ]);
+
+        Recipe::factory()->create([
+            'id'          => 3,
+            'title'       => 'Gulasch',
+            'ingredients' => ['Meat', 'Onions', 'Broth', 'Tomato paste', 'Paprika powder', 'Butter', 'Thyme', 'Majoran', 'Salt', 'Pepper'],
+            'preparation' => 'Fry the Meat for 10 minutes and then add the onions. After about 10 more minutes, add the tomato paste, mix it, and add the paprika powder. Mix the Butter with the Spices and add it to the pot. Cook it for about 90 minutes.',
+            'difficulty'  => 'medium',
+            'time'        => '120',
         ]);
 
         Recipe::factory(100)->create();
--- resources/js/Pages/Recipes.vue
+++ resources/js/Pages/Recipes.vue
@@ -28,35 +28,6 @@ defineProps(['recipes']);
                         :time="recipe.time"
                     />
                 </li>
-                <!--
-                <li>
-                    <Recipe
-                        title="Spaghetti Carbonara"
-                        :ingredients="['Spaghetti', 'Eggs', 'Parmesan', 'Bacon', 'Black Pepper']"
-                        preparation="Boil spaghetti. Cook bacon. Mix eggs with parmesan and combine with cooked spaghetti and bacon."
-                        difficulty="Medium"
-                        :time="30"
-                    />
-                </li>
-                <li>
-                    <Recipe
-                        title="Pancakes"
-                        :ingredients="['Milk', 'Flour', 'Eggs', 'Butter']"
-                        preparation="Mix everything and then fry it in a pan."
-                        difficulty="Easy"
-                        :time="10"
-                    />
-                </li>
-                <li>
-                    <Recipe
-                        title="Gulasch"
-                        :ingredients="['Meat', 'Onions', 'Broth', 'Tomato paste', 'Paprika powder', 'Butter', 'Thyme', 'Majoran', 'Salt', 'Pepper']"
-                        preparation="Fry the Meat for 10 minutes and then add the onions. After about 10 more minutes, add the tomato paste, mix it, and add the paprika powder. Mix the Butter with the Spices and add it to the pot. Cook it for about 90 minutes."
-                        difficulty="Medium"
-                        :time="120"
-                    />
-                </li>
-                -->
             </ul>
         </div>
     </AuthenticatedLayout>
