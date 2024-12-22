686ca021945cb383927a038ff5c06bd5b7e09a31
mls42 <laspina92@gmail.com>
2024-12-22 12:29:58 +0100

    Add Recipe component

---
+++ resources/js/Components/Recipe.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="recipe">
+        <div class="recipe-head">
+            <h2 class="recipe-title">{{ title }}</h2>
+            <div class="recipe-difficulty">{{ difficulty }}</div>
+            <div class="recipe-time">{{ time }} min</div>
+        </div>
+        <ul class="recipe-ingredients">
+            <li v-for="ingredient in ingredients" :key="ingredient">{{ ingredient }}</li>
+        </ul>
+        <div class="recipe-preparation">{{ preparation }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Recipe',
+        props: {
+            title: {
+                type: String,
+                required: true,
+            },
+            ingredients: {
+                type: Array,
+                required: true,
+            },
+            preparation: {
+                type: String,
+                required: true,
+            },
+            difficulty: {
+                type: String,
+                required: true,
+            },
+            time: {
+                type: Number,
+                required: true,
+            },
+        },
+    };
+</script>
--- resources/js/Pages/Recipes.vue
+++ resources/js/Pages/Recipes.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Recipe from '../Components/Recipe.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
@@ -14,5 +15,32 @@ import { Head } from '@inertiajs/vue3';
                 Recipes
             </h2>
         </template>
+        <div class="content-wrapper">
+            <ul class="recipe-list">
+                <li>
+                    <Recipe
+                        title="Spaghetti Carbonara"
+                        :ingredients="['Spaghetti', 'Eggs', 'Parmesan', 'Bacon', 'Black Pepper']"
+                        preparation="Boil spaghetti. Cook bacon. Mix eggs with parmesan and combine with cooked spaghetti and bacon."
+                        difficulty="Medium"
+                        :time="30"
+                    />
+                    <Recipe
+                        title="Pancakes"
+                        :ingredients="['Milk', 'Flour', 'Eggs', 'Butter']"
+                        preparation="Mix everything and then fry it in a pan."
+                        difficulty="Easy"
+                        :time="10"
+                    />
+                    <Recipe
+                        title="Gulasch"
+                        :ingredients="['Meat', 'Onions', 'Broth', 'Tomato paste', 'Paprika powder', 'Butter', 'Thyme', 'Majoran', 'Salt', 'Pepper']"
+                        preparation="Fry the Meat for 10 minutes and then add the onions. After about 10 more minutes, add the tomato paste, mix it, and add the paprika powder. Mix the Butter with the Spices and add it to the pot. Cook it for about 90 minutes."
+                        difficulty="Medium"
+                        :time="120"
+                    />
+                </li>
+            </ul>
+        </div>
     </AuthenticatedLayout>
 </template>
